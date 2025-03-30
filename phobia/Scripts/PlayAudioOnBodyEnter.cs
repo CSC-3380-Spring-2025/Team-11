19247f072f6cf31eb0c9e0bab7d412186e8f6ab4
Brent Bolden <brentboldenjr2@gmail.com>
2025-03-29 20:23:47 -0500

    Added Situational Sounds
    Added a node that plays an audio when a collision object in the object's collision mask collides with the area. It audio can be set to play continuously and a timer can be set to prevent the sound from being played too many times

---
+++ phobia/Scripts/PlayAudioOnBodyEnter.cs
@@ -0,0 +1,66 @@
+using Godot;
+using System;
+
+//Audio plays whenever an object in the area's collision mask is entered. 
+public partial class PlayAudioOnBodyEnter : Area3D
+{
+
+	[Export]
+	public bool continuous;
+	[Export]
+	public Timer cooldownTimer;
+	[Export]
+	public AudioStreamPlayer3D audio;
+	private bool audioPlaying = false;
+	private bool bodiesInArea = false;
+	
+	
+
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+		cooldownTimer.Autostart = false;
+		cooldownTimer.OneShot = true;
+		BodyEntered += OnBodyEntered; 
+		BodyExited += OnBodyExited;
+		audio.Finished += OnAudioFinished;
+	}
+
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta)
+	{
+	}
+
+	private void OnBodyEntered(Node3D body)
+	{
+		if(cooldownTimer.TimeLeft == 0 && !bodiesInArea)
+		{
+			if(continuous)
+			{
+				audioPlaying = true;
+			}
+
+			bodiesInArea = true;
+			audio.Play();
+			cooldownTimer.Start();
+		}
+	}
+
+	private void OnBodyExited(Node3D body)
+	{
+		if(GetOverlappingBodies().Count <= 0)
+		{
+		audioPlaying = false;
+		bodiesInArea = false;
+		}
+
+
+	}
+	private void OnAudioFinished()
+	{
+		if(continuous && audioPlaying)
+		{
+			audio.Play();
+		}
+	}
+}
